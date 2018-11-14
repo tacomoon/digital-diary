44bf65b574dddbff085486e55434bbca6f19e45f
tacomoon <evlgorbachev@gmail.com>
2018-11-14 09:54:49 +0300

    Move some logic in database shell script to functions

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,54 +1,73 @@
 #!/bin/bash
 
-# TODO function to find container id
+function find_running_container {
+    CONTAINER_ID=$(docker ps -q -f=ancestor=digital-diary-database:latest)
+}
+
+function check_container_running {
+    if [ -z ${CONTAINER_ID} ]; then
+        find_running_container
+    fi
+
+    if [ -z ${CONTAINER_ID} ]; then
+        echo "Can't find running digital-diary database container"
+        return 1
+    fi
+}
+
+function soft_stop_container {
+    find_running_container
+
+    if [ ${CONTAINER_ID} ]; then
+        echo "Stopping running container $CONTAINER_ID"
+        docker stop ${CONTAINER_ID}
+    fi
+}
+
 case ${1} in
 
     b | build )
         VERSION=$(date +'%s')
-        DOCKER_CONTEXT="$(dirname "$0")/../database"
+        DOCKER_CONTEXT=$(dirname "$0")/../database
+
         docker build -t digital-diary-database:${VERSION} -f ${DOCKER_CONTEXT}/Dockerfile ${DOCKER_CONTEXT}
         docker tag digital-diary-database:${VERSION} digital-diary-database:latest
         ;;
 
     r | run )
-        CONTAINER_ID=$(docker ps -q -f=ancestor=digital-diary-database:latest)
-        if [[ ${CONTAINER_ID} ]]; then
-            echo "Stopping running container $CONTAINER_ID"
-            docker stop ${CONTAINER_ID}
-        fi
-
+        soft_stop_container
         echo "Running latest digital-diary database container"
         docker run -d -p 5432:5432 -v digital-diary:/var/lib/postgresql/data digital-diary-database:latest
         ;;
 
     c | connect )
-        CONTAINER_ID=$(docker ps -q -f=ancestor=digital-diary-database:latest)
-        if [ -z "${CONTAINER_ID}" ]; then
-            echo "Can't find running digital-diary database container"
-        else
+        check_container_running
+        if [ ${CONTAINER_ID} ]; then
             echo "Running psql in container $CONTAINER_ID"
             docker exec -it ${CONTAINER_ID} bash -c "psql -U development -d digital-diary"
         fi
         ;;
 
     s | stop )
-        CONTAINER_ID=$(docker ps -q -f=ancestor=digital-diary-database:latest)
-        if [[ -z ${CONTAINER_ID} ]]; then
-            echo "Can't find running digital-diary database container"
-        else
-            echo "Stopping container $CONTAINER_ID"
-            docker stop ${CONTAINER_ID}
-        fi
+        check_container_running
+        soft_stop_container
         ;;
 
     clean )
+        soft_stop_container
+
         echo "Removing all related images and containers"
         docker rm $(docker ps -qa -f ancestor=digital-diary-database) 2> /dev/null
         docker rmi -f $(docker images -qa digital-diary-database) 2> /dev/null
         ;;
 
     * )
-        echo -e "Basic actions with database container"
+        if [ $1 ]; then
+            echo "Unknown command: $1"
+        else
+            echo -e "Basic actions with database container"
+        fi
+
         echo -e "\nCommands:"
         echo -e "  b, build\tBuild and tag image as latest"
         echo -e "  r, run\tRun pg database image tagged as latest"
