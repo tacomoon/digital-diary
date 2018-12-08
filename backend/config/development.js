03def3b78dce256b8524f43cb8ad402cb073c6da
tacomoon <evlgorbachev@gmail.com>
2018-12-08 21:48:22 +0300

    Update logger setting so now whole error message will be colored

--- backend/config/development.js
+++ backend/config/development.js
@@ -2,10 +2,19 @@
 
 const { raw } = require('config/raw')
 const { format, transports } = require('winston')
-const { combine, simple, splat, timestamp, colorize, printf } = format
+const { combine, simple, splat, timestamp, printf } = format
 
+const colorizer = format.colorize()
 const timestampFormat = timestamp({ format: 'YYYY-MM-DD HH:mm:ss' })
-const messageFormat = printf(({ message, timestamp, level }) => `${timestamp} ${level}: ${message}`)
+const messageFormat = printf(({ message, timestamp, level }) => {
+  switch (level) {
+    case 'error':
+      return colorizer.colorize(level, `${timestamp} ${level}: ${message}`)
+    default:
+      return `${timestamp} ` + colorizer.colorize(level, `${level}: `) + message
+
+  }
+})
 
 module.exports = {
   express: {
@@ -40,7 +49,7 @@ module.exports = {
   logger: {
     console: raw({
       level: 'info',
-      format: combine(simple(), splat(), colorize(), timestampFormat, messageFormat),
+      format: combine(simple(), splat(), timestampFormat, messageFormat),
       transports: [
         new transports.Console()
       ]
